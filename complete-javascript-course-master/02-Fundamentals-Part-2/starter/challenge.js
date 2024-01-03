55ad5535ff8e3337416855a6cbfa92c71cbc0944
GwanHooPark <hooho.dev@gmail.com>
2024-01-03 20:32:15 +0900

    함수 첼린지1

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge.js
@@ -0,0 +1,68 @@
+// 공식 1
+// const 변수(매개변수) {
+//     return();
+// }
+
+
+// 공식 2
+// const 변수 = function(매개변수) {
+//     return();
+// }
+
+
+// 공식 3
+// const 변수 = 매개변수 => 
+
+
+// const dolphins;
+// const koalas;
+
+/* 
+const calcAverage => scoreDolphins() ? scoreKoalas();
+
+const data1 = dolphins(44 + 23 + 71 / 3);
+const date1 = koalas(65 + 54 + 49 /3); 
+
+const data2 = dolphins(85 + 54 + 41 / 3);
+const date2 = koalas(23 + 34 + 27 /3);  
+*/
+
+
+// 1. 평균 점수를 내라
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage (3, 4, 5));
+
+// const calcAverage = dolphins(44 + 23 + 71) + koalas(65 + 54 + 49) / 3;
+// console.log(calcAverage);
+
+
+// 2. 양 팀의 평균을 계산
+// 테스트 데이터 1
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+
+// 3.
+const checkWinner = function(avgDolphins, avgKoalas) {
+    // return 30 vs. 13;
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`돌고래가 이겼다. (${avgDolphins} vs ${avgKoalas})`)
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`코알라가 이겼다. (${avgKoalas} vs ${avgDolphins})`)
+    } else {
+        console.log(`'No team wins...' ${avgDolphins} vs ${avgKoalas}`)
+    }
+}
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+// 테스트 데이터 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+
