932c83b547bd582101e54f5f09d7b2e214611019
hyeju7979 <hyeju7979@naver.com>
2024-02-01 17:55:27 +0900

    todolist 정규식

--- TodoList/todolist.js
+++ TodoList/todolist.js
@@ -13,10 +13,13 @@ function checkBox(e) {
         $(this).parent().css('text-decoration','');
     }
     */
-    if($(".checkRun").is(":checked")){
-        $(this).parent().addClass('strikethrough');       
+
+    //$(this).closest('li').toggleClass('strikethrough');  
+
+    if($(this).is(":checked")){
+        $(this).closest('li').addClass('strikethrough');       
     }else{
-        $(this).parent().removeClass('strikethrough');
+        $(this).closest('li').removeClass('strikethrough');
     }
 }
 
@@ -38,26 +41,27 @@ function addTodo() {
         alert('내용을 입력하세요!');
         return;
     }    
-
-    // 특수문자 정규식 변수(공백 미포함)
-    const replaceChar = /[~!@\#$%^&*\()\-=+_'\;<>0-9\/.\`:\"\\,\[\]?|{}]/gi;
  
     // 완성형 아닌 한글 정규식
-    const replaceNotFullKorean = /[ㄱ-ㅎㅏ-ㅣ]/gi;
-
-    $("#todoInput").on("focusout", function() {
-        var x = $(this).val();
-        if (x.length > 0) {
-            if (x.match(replaceChar) || x.match(replaceNotFullKorean)) {
-                x = x.replace(replaceChar, "").replace(replaceNotFullKorean, "");
-            }
-            $(this).val(x);
-        }
-        }).on("keyup", function() {
-            $(this).val($(this).val().replace(replaceChar, ""));
-
-   });
-
+    const regNumber = /[0-9]/gi;
+    if(regNumber.test(inputValue)){
+        alert('숫자들어가면 안돼!!!');
+        $('#todoInput').val('');
+        return;
+    }
+    
     $('#addTodo').append('<li><input type="checkbox" class="checkRun">'+inputValue+'<button class="removeBtn">삭제</button></li>');
     $('#todoInput').val('');
+
+
+    /** 
+    
+        1. 인풋 앞에 셀렉트 박스를 추가한다.
+        2. 이 셀렉트 박스는 두가지인데 1)카테고리 2)집안일 3)회사일
+        3. 카테고리를 선택하고 할일을 입력한 후 추가하면
+        4. 결과로 [카테고리] 입력한 내용
+        5. 만약 카테고리를 선택하지 않고 추가를 하면 경고창을 띄워준다.
+        
+
+    */
 }
